ecbd6bb7d6c01a7a63e622151465aa8ffb4a7a93
AnnA <anahita2012rad@gmail.com>
2023-10-03 22:04:51 +0200

    calender

---
+++ src/main/java/se/lexicon/App.java
@@ -0,0 +1,37 @@
+package se.lexicon;
+
+import se.lexicon.dao.UserDao;
+import se.lexicon.dao.impl.UserDaoImpl;
+import se.lexicon.dao.impl.db.MeetingCalendarDBConnection;
+import se.lexicon.exception.AuthenticationFailedException;
+import se.lexicon.exception.CalendarExceptionHandler;
+import se.lexicon.exception.UserExpiredException;
+import se.lexicon.model.User;
+
+import java.util.Optional;
+
+public class App {
+    public static void main(String[] args) {
+
+        try {
+            UserDao userDao = new UserDaoImpl(MeetingCalendarDBConnection.getConnection());
+            User user = new User("admin");
+            User createdUser = userDao.createUser(user.getUsername());
+            System.out.println("CratedUser.userInfo() + " + createdUser.userInfo());
+            Optional<User> userOptional = userDao.findByUserName("admin");
+            if (userOptional.isPresent()) {
+                System.out.println(userOptional.get().userInfo());
+            }
+
+            try {
+                userDao.authenticate(new User("admin", "123456"));
+            } catch (AuthenticationFailedException e) {
+                System.out.println(e.getMessage());
+            } catch (UserExpiredException e) {
+                System.out.println(e.getMessage());
+            }
+        } catch (Exception e) {
+            CalendarExceptionHandler.handelException(e);
+        }
+    }
+}
---
+++ src/main/java/se/lexicon/dao/UserDao.java
@@ -0,0 +1,19 @@
+package se.lexicon.dao;
+
+import se.lexicon.exception.AuthenticationFailedException;
+import se.lexicon.exception.MySQLException;
+import se.lexicon.exception.UserExpiredException;
+import se.lexicon.model.User;
+
+import java.util.Optional;
+
+public interface UserDao {
+    User createUser(String username);
+
+    Optional<User> findByUserName(String username);
+
+    boolean removeUser(String username);
+
+    boolean authenticate(User user) throws AuthenticationFailedException, UserExpiredException;
+}
+
